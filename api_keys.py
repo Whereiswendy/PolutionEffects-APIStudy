40e03d3a3c30955773193225a28b8b25799b5ec1
Wendy <wendy.ware2@gmail.com>
2024-04-25 13:40:16 +0800

    WW first attempt at working with APIs

---
+++ api_keys.py
@@ -0,0 +1,12 @@
+# OpenWeatherMap API Key
+weather_api_key = "e5692f2136e4a97d76ad793471541140"
+
+# Geoapify API Key
+geoapify_key = "c89e0d1e6285437c959cec653ddf659b"
+
+# World Air Quality Index (AQICN) API Key
+aqicn_api_key = "403ad2e9e10fed0a66e469938e9e5034ec2b99bf"
+
+# Global Health Observatory (GHO) by WHO Key
+gho_who_api_key = "PLACE KEY HERE"
+
